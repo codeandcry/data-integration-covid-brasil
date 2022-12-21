6f7b1bca48e35363e4d4ced613789c4d902b5d96
Lyndemberg Batista <lyndemberg1batista@gmail.com>
2022-12-21 00:00:01 -0300

    add dag processing structure

--- dags/processing_datasets.py
+++ dags/processing_datasets.py
@@ -8,6 +8,10 @@ import pendulum
 from airflow import DAG
 from airflow.decorators import task
 from minio import Minio
+from io import BytesIO
+import pandas as pd
+from sqlalchemy import create_engine
+from airflow.hooks.base import BaseHook
 
 log = logging.getLogger(__name__)
 
@@ -25,34 +29,68 @@ with DAG(
     tags=["processing"],
 ) as dag:
 
-    @task(task_id="check-minio-connection")
+    @task(task_id="check_minio_connection")
     def check_minio_connection(ds=None, **kwargs):
         client.list_buckets()
         print("Conexão ativa!")
     check_minio_connection_step = check_minio_connection()
 
-    @task(task_id="get-ngl-output-file")
-    def get_ngl_output_file(ds=None, **kwargs):
-        arquivo = client.get_object("ngl-output", "ngl-output.csv")
-        df = pd.read_csv(BytesIO(arquivo.read()))
-        print(df.head())
-    get_ngl_output_file_step = get_ngl_output_file()
+    @task(task_id="check_data_warehouse_connection")
+    def checkDataWarehouseConnection():
+        dataWarehouseConn = BaseHook.get_connection('data_warehouse_connection')
+        cnx = create_engine(
+            dataWarehouseConn.schema + '://' + dataWarehouseConn.login + ':' + dataWarehouseConn.password + '@' + dataWarehouseConn.host)
+        cnx.execute('select @@version as version;')
+    check_data_warehouse_connection_step = checkDataWarehouseConnection()
 
-    @task(task_id="get-srag-output-file")
-    def get_srag_output_file(ds=None, **kwargs):
-        arquivo = client.get_object("srag-output", "srag-output.csv")
-        df = pd.read_csv(BytesIO(arquivo.read()))
-        print(df.head())
-    get_srag_output_file_step = get_srag_output_file()
+    def processBucketFilesToSingleDataFrame(bucketName):
+        dfAll = pd.DataFrame()
+        for item in client.list_objects(bucketName, recursive=True):
+            print(f'Processing {item.object_name} in bucket={bucketName}')
+            file = client.get_object(bucketName, item.object_name, item.object_name)
+            df = pd.read_csv(BytesIO(file.read()), sep=";", encoding='utf-8', low_memory=False)
+            print(f'{len(df)} rows in "{item.object_name}')
+            dfAll = pd.concat([dfAll, df], ignore_index=True, sort=False)
 
-    @task(task_id="get-vacinacao-output-file")
-    def get_vacinacao_output_file(ds=None, **kwargs):
-        arquivo = client.get_object("vacinacao-output", "vacinacao-output.csv")
-        df = pd.read_csv(BytesIO(arquivo.read()))
-        print(df.head())
-    get_vacinacao_output_file_step = get_vacinacao_output_file()
+        dfAll.fillna('', inplace=True)
+        print(f'bucket: {bucketName} rows: {len(dfAll)}')
+        return dfAll
+
+    @task(task_id="get_ngl_output")
+    def getNglOutput():
+        return processBucketFilesToSingleDataFrame("ngl-output")
+    get_ngl_output_step = getNglOutput()
+
+    @task(task_id="get_srag_output")
+    def getSragOutput():
+        return processBucketFilesToSingleDataFrame("srag-output")
+    get_srag_output_step = getSragOutput()
+
+    @task(task_id="get_vacinacao_output")
+    def getVacinacaoOutput():
+        return processBucketFilesToSingleDataFrame("vacinacao-output")
+    get_vacinacao_output_step = getVacinacaoOutput()
+
+    @task(task_id="merge_datasets")
+    def mergeDatasets(**kwargs):
+        ti = kwargs['ti']
+        dfNglRetorno = ti.xcom_pull(task_ids='get_ngl_output')
+        dfSragRetorno = ti.xcom_pull(task_ids='get_srag_output')
+        dfVacinacaoRetorno = ti.xcom_pull(task_ids='get_vacinacao_output')
+        print(f'count {len(dfNglRetorno)}')
+        print(f'count {len(dfSragRetorno)}')
+        print(f'count {len(dfVacinacaoRetorno)}')
+        print(f'merge datasets executed')
+    merge_data_step = mergeDatasets()
+
+    # @task(task_id="publish_to_data_warehouse")
+    # def publishToDataWarehouse(tableName, dataFrame):
+    #     mysql_connection = BaseHook.get_connection('data_warehouse_connection')
+    #     cnx = create_engine(
+    #         mysql_connection.schema + '://' + mysql_connection.login + ':' + mysql_connection.password + '@' + mysql_connection.host)
+    #     dataFrame.to_sql(name=tableName, con=cnx, if_exists='replace')
+    # publish_to_data_warehouse_step = publishToDataWarehouse()
 
 
-check_minio_connection_step >> get_ngl_output_file_step
-get_ngl_output_file_step >> get_srag_output_file_step
-get_srag_output_file_step >> get_vacinacao_output_file_step
+
+check_minio_connection_step >> get_ngl_output_step >> get_srag_output_step >> get_vacinacao_output_step >> merge_data_step >> check_data_warehouse_connection_step
